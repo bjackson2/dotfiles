c740bfea9865939dc7670c0ff64ead2371229ce1
Ben Jackson <bjackson2@gmail.com>
2023-11-14 09:50:42 -0500

    Disables auto-format on save for eslint

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -17,13 +17,6 @@ cmp.setup({
 })
 
 require'lspconfig'.tsserver.setup{}
-require'lspconfig'.eslint.setup({
-  on_attach = function(client, bufnr)
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      buffer = bufnr,
-      command = "EslintFixAll",
-    })
-  end,
-})
+require'lspconfig'.eslint.setup({})
 require'lspconfig'.solargraph.setup{}
 
