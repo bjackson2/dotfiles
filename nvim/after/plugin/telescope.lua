7a3de74309c0c9dc4187da6aa176577b780b7d4c
Ben Jackson <bjackson2@gmail.com>
2023-11-13 20:48:56 -0500

    Adds some themes for Telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,5 +1,17 @@
 local builtin = require('telescope.builtin')
+local dropdown_options = {
+  layout_config = {
+    preview_cutoff = 1,
+    width = function(_, max_columns, _)
+      return math.min(max_columns, 150)
+    end,
 
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+    height = function(_, _, max_lines)
+      return math.min(max_lines, 20)
+    end,
+  },
+}
+
+vim.keymap.set('n', '<leader>pf', function() builtin.find_files(require('telescope.themes').get_dropdown(dropdown_options)) end, {})
+vim.keymap.set('n', '<C-p>', function() builtin.git_files(require('telescope.themes').get_dropdown(dropdown_options)) end, {})
+vim.keymap.set('n', '<leader>ps', function() builtin.live_grep(require('telescope.themes').get_dropdown(dropdown_options)) end, {})
