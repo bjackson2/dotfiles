12768213a0e8147a37fcd3dbfcdad804fec295fb
Ben Jackson <bjackson2@gmail.com>
2023-12-12 09:49:33 -0500

    Adds leader command to search word under cursor within file

--- nvim/lua/core/remap.lua
+++ nvim/lua/core/remap.lua
@@ -24,6 +24,8 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>/", "/<C-R><C-W><cr>")
+
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
