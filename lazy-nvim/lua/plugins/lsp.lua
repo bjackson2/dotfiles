dc325879f29eee54db7dfa1c7f6fef7f6757a97f
Ben Jackson <bjackson2@gmail.com>
2024-01-15 21:20:36 -0500

    Adds Lazy-nvim config

---
+++ lazy-nvim/lua/plugins/lsp.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "bash-language-server",
+        "css-lsp",
+        "eslint-lsp",
+        "eslint_d",
+        "solargraph",
+        "typescript-language-server",
+      })
+    end,
+  },
+}
