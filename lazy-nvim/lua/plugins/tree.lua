585620ac9a72e683f5e9e5e8cd4d3e7c876e7bb6
Ben Jackson <bjackson2@gmail.com>
2024-05-22 14:57:24 -0400

    Reveal file in finder

--- lazy-nvim/lua/plugins/tree.lua
+++ lazy-nvim/lua/plugins/tree.lua
@@ -1,6 +1,12 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
+    vim.keymap.set(
+      "n",
+      "<leader>fi",
+      ":Neotree filesystem reveal<CR>",
+      { silent = true, desc = "Show file in finder" }
+    ),
     opts = {
       filesystem = {
         follow_current_file = { enabled = false },
