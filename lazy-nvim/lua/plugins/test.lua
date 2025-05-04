16cd34ea48a43fed863894c6144910d89b663377
Ben Jackson <bjackson2@gmail.com>
2025-05-04 14:51:38 -0400

    Adds windsurf settings, avante nvim

--- lazy-nvim/lua/plugins/test.lua
+++ lazy-nvim/lua/plugins/test.lua
@@ -4,8 +4,8 @@ return {
     dependencies = {
       "preservim/vimux",
     },
-    vim.keymap.set("n", "<leader>t", ":TestNearest<CR>", { silent = true, desc = "Run current test" }),
-    vim.keymap.set("n", "<leader>a", ":TestFile<CR>", { silent = true, desc = "Run all tests in file" }),
+    vim.keymap.set("n", "<leader>tt", ":TestNearest<CR>", { silent = true, desc = "Run current test" }),
+    vim.keymap.set("n", "<leader>ta", ":TestFile<CR>", { silent = true, desc = "Run all tests in file" }),
     vim.cmd([[let test#strategy = 'vimux']]),
   },
 }
