35be93c528055261c4c4ff5495f204f8725f9419
Ben Jackson <bjackson2@gmail.com>
2025-12-22 12:26:17 -0500

    ghostty colors, buffer save on exit

--- lazy-nvim-v2/lua/config/autocmds.lua
+++ lazy-nvim-v2/lua/config/autocmds.lua
@@ -6,3 +6,15 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Auto-save when focus is lost
+vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave" }, {
+  pattern = "*",
+  callback = function()
+    -- Only save if buffer is modified and has a filename
+    if vim.bo.modified and vim.fn.expand("%") ~= "" then
+      vim.cmd("silent! update")
+    end
+  end,
+  desc = "Auto-save on focus lost",
+})
